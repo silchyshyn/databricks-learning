21c02a36a07f7a5093d7dab05d6e7cd13a5a984d
silchyshyn <silchyshyn>
2023-01-28 17:52:53 +0000

    first commit

---
+++ test_notebook.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %md <i18n value="bfb2d018-5d5a-4475-bf1e-293e2a5b0100"/>
+# MAGIC 
+# MAGIC # Data Engineering with Databricks
+# MAGIC 
+# MAGIC This course prepares you for topics in the Databricks Certified Associate Data Engineer certification exam.
+# MAGIC 
+# MAGIC Data professionals from all walks of life will benefit from this comprehensive introduction to the components of the Databricks Lakehouse Platform that directly support putting ETL pipelines into production. You will leverage SQL and Python to define and schedule pipelines that incrementally process new data from a variety of data sources to power analytic applications and dashboards in the Lakehouse. This course offers hands-on instruction in Databricks Data Science & Engineering Workspace, Databricks SQL, Delta Live Tables, Databricks Repos, Databricks Task Orchestration, and the Unity Catalog.
+# MAGIC 
+# MAGIC **Duration:** 2 full days or 4 half days
+# MAGIC 
+# MAGIC #### Objectives
+# MAGIC - Leverage the Databricks Lakehouse Platform to perform core responsibilities for data pipeline development
+# MAGIC - Use SQL and Python to write production data pipelines to extract, transform, and load data into tables and views in the Lakehouse
+# MAGIC - Simplify data ingestion and incremental change propagation using Databricks-native features and syntax, including Delta Live Tables
+# MAGIC - Orchestrate production pipelines to deliver fresh results for ad-hoc analytics and dashboarding
+# MAGIC 
+# MAGIC #### Prerequisites
+# MAGIC - Basic knowledge of SQL query syntax, including writing queries using `SELECT`, `WHERE`, `GROUP BY`, `ORDER BY`, `LIMIT`, and `JOIN`
+# MAGIC - Basic knowledge of SQL DDL statements to create, alter, and drop databases and tables
+# MAGIC - Basic knowledge of SQL DML statements, including `DELETE`, `INSERT`, `UPDATE`, and `MERGE`
+# MAGIC - Experience with or knowledge of data engineering practices on cloud platforms, including cloud features such as virtual machines, object storage, identity management, and metastores
